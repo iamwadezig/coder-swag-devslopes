8a5ecee320393ddf594d49bf2eac209b19d95866
Ario Nugroho <gamerzdope@gmail.com>
2019-08-17 16:36:27 +0700

    Initial Commit

---
+++ coder-swag@devslopes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  coder-swag@devslopes
+//
+//  Created by Ario Nugroho on 17/08/19.
+//  Copyright © 2019 Ario Nugroho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
